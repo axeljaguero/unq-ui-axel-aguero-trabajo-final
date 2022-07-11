86a8b59c4f4a855b31eb75ccb8e6ed264a791c4c
Axel Agüero <axeljaguero@gmail.com>
2022-07-11 15:22:28 -0300

    parte logica del juego

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,43 +1,129 @@
-import React from 'react';
+import { React, useState, useEffect} from 'react';
 import './style.css';
 
-class Home extends React.Component {
-    render() {
-        return (
-        <div>
-            <h1>Welcome to Rock-Paper-Scissors-Lizard-Spock!</h1>
-            <p>
-            This is a game of rock, paper, scissors, lizard, Spock.
-            </p>
-            <p>
-            The rules are simple: Rock beats scissors, paper beats rock, scissors beats lizard, lizard beats Spock, Spock beats paper, and paper beats rock.
-            </p>
-            <p>
-            The game is played with a computer opponent. The computer opponent will randomly choose a move. The player will then choose a move and the winner will be determined.
-            </p>
-            <p>
-            The game will be played with a score board. The player will have a score of wins and the computer will have a score of losses. The winner will be the one with the most wins.
-            </p>
-            <p>
-            The game will also have a history of moves. The player will have a history of moves and the computer will have a history of moves. The history will be displayed on the screen.
-            </p>
-            <p>
-            The game will also have a history of wins and losses. The player will have a history of wins and the computer will have a history of losses. The history will be displayed on the screen.
-            </p>
-            <p>
-            The game will also have a history of ties. The player will have a history of ties and the computer will have a history of ties. The history will be displayed on the screen.
-            </p>
-            <p>
-            The game will also have a history of ties. The player will have a history of ties and the computer will have a history of ties. The history will be displayed on the screen.
-            </p>
-            <p>
-            The game will also have a history of ties. The player will have a history of ties and the computer will have a history of ties. The history will be displayed on the screen.
-            </p>
-            <p>
-            The game will also have a history of ties. The player will have a history of ties and the computer will have a history of ties. The history will be displayed on the screen.
-            </p>
+const Home = () => {
+
+    const [humanChoice, setHumanChoice] = useState(null);
+    const [computerChoice, setComputerChoice] = useState(null);
+    const [result, setResult] = useState(null);
+    const [score, setScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
+    const possibleChoice = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+
+    const randomChoice = () => {
+        const randomNumber = Math.floor(Math.random() * 5);
+        return possibleChoice[randomNumber];
+    }
+
+    const humanChoiceHandler = (event) => {
+        setHumanChoice(event.target.value);
+        computerChoiceHandler();
+    }
+
+    const computerChoiceHandler = () => {
+        setComputerChoice(randomChoice());
+    }
+
+    const compare = () => {
+        if (humanChoice === computerChoice) {
+            setResult('Draw');
+        } else if (
+            (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+            (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+            (humanChoice === 'Scissors' && computerChoice === 'Paper') ||
+            (humanChoice === 'Lizard' && computerChoice === 'Paper') ||
+            (humanChoice === 'Spock' && computerChoice === 'Rock')
+        ) {
+            setResult('You win');
+            setScore(score + 1);
+        } else {
+            setResult('You lose');
+            setComputerScore(computerScore + 1);
+        }
+    }
+
+    const restartGame = () => {
+        setHumanChoice(null);
+        setComputerChoice(null);
+        setResult(null);
+        setComputerScore(0);
+        setScore(0);
+    }
+
+    useEffect(() => {
+        if (humanChoice && computerChoice) {
+            compare();
+        }
+    }, [humanChoice, computerChoice])
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Your Choice</h5>
+                            <p className="card-text">
+                                {humanChoice}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Computer Choice</h5>
+                            <p className="card-text">
+                                {computerChoice}
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )}
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Result</h5>
+                            <p className="card-text">
+                                {result}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Score</h5>
+                            <p className="card-text">
+                                You: {score}
+                                <br />
+                                Computer: {computerScore}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    {possibleChoice.map((choice, index) => {
+                        return(
+                            <button key={index} className="btn btn-primary" onClick={humanChoiceHandler} value={choice}>
+                                {choice}
+                            </button>
+                        )
+                    }
+                    )}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <button className="btn btn-primary" onClick={restartGame}>Restart</button>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Home;
